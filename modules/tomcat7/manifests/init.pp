d401a8adbfb9bab631db0f7afeeb03ce6b79e29b
Felix Gilcher <felix.gilcher@asquera.de>
2013-02-03 13:04:38 +0100

    ensure that directories are created with the right permissions

--- modules/tomcat7/manifests/init.pp
+++ modules/tomcat7/manifests/init.pp
@@ -4,10 +4,24 @@ class tomcat7 {
   package { "tomcat7":
     ensure => installed,
     require => [Yumrepo['jpackage']]
-  }
-
-  service { "tomcat7":
-    ensure => started,
+  } -> file { "/var/log/tomcat7":
+    ensure => directory,
+    mode => 0775,
+    owner => "root",
+    group => "tomcat",
+  } -> file { "/var/cache/tomcat7/temp":
+    ensure => directory,
+    mode => 0775,
+    owner => "root",
+    group => "tomcat",
+  } -> file { "/var/lib/tomcat7/webapps":
+    ensure => directory,
+    mode => 0775,
+    owner => "root",
+    group => "tomcat",
+  } -> service { "tomcat7":
+    ensure => running,
     require => [Package['tomcat7']]
   }
+  
 }
